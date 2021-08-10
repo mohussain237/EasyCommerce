42f9652b1748122c8ccb70757374aedb33bb7258
mohammed Hussain <mohussain237@gmail.com>
2021-08-10 19:40:55 +0530

    creating separate  rating component

---
+++ src/Components/Rating.js
@@ -0,0 +1,83 @@
+import React from "react";
+// import PropTypes from "prop-Type";
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+        <span> {text && text}</span>
+      </span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "brown",
+};
+
+// Rating.propType ={
+//     value:PropType.number.isRequired
+//     text:PropType.string.isRequired
+//     color:PropType.string.
+
+// }
+
+export default Rating;
