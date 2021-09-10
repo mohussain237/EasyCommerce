f7502d4c4d76fbcf2b34633ca11222a484583452
Mohammed Hussian <mohussain237@gmail.com>
2021-09-10 07:12:51 +0100

    adding new add to cart route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
+import CartScreen from "./Screens/CartScreen"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path = "/cart/:id?" component={CartScreen}/>
         </Container>
       </main>
 
