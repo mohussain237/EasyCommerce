7a84bd8f3f480c611533041f0d95906f73bc80ed
Mohammed Hussian <mohussain237@gmail.com>
2021-09-10 07:14:10 +0100

    creating cart item store

--- src/store.js
+++ src/store.js
@@ -4,13 +4,24 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import {
   productDetailsReducers,
   productListReducers,
+
 } from "./reducers/productReducers";
+import { cartReducers } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducers,
+  cart:cartReducers
+
 });
-const initialStage = {};
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialStage = {
+  cart: {cartItems :cartItemsFromStorage}
+};
 const middleware = [thunk];
 
 const store = createStore(
